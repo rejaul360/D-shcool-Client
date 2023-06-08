a70d6767267c2bbeaf7747980b8900b8d22d1fdc
rejaul360 <rejaraju2k22@gmail.com>
2023-06-09 04:28:22 +0600

    implement admin route

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -1,18 +1,22 @@
 import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 
 const useAdmin = () => {
-    const {user} = useContext(AuthContext)
+    const {user,loading} = useContext(AuthContext);
+    const [axiosSecure] = useAxiosSecure();
     const {data: isAdmin, isLoading: isAdminLoading} = useQuery({
         queryKey: ['isAdmin', user?.email],
         enabled: !loading,
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000//users/admin/${user?.email}`);
+            const res = await axiosSecure.get(`/users/admin/${user?.email}`);
             // console.log('is admin response', res)
             return res.data.admin;
         }
     })
+    return [isAdmin, isAdminLoading]
 };
 
 export default useAdmin;
--- src/Main/Dashboard.jsx
+++ src/Main/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { FaHome, FaCheckCircle, FaWallet, FaEnvelopeOpenText } from 'react-icons/fa';
 import { NavLink, Outlet } from 'react-router-dom';
+import useAdmin from '../Hooks/useAdmin';
 
 
 const Dashboard = () => {
 
-    const isAdmin = true;
+    const [isAdmin] = useAdmin()
+
+    // const isAdmin = true;
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
